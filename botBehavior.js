b65fde5db5414593cfe984a1120c8c1a03e6bd5e
SkrienaSenka <shadow26.hm@gmail.com>
2023-01-03 19:16:53 +0100

    Prevent fuckers from using commands and add terminal logs

--- botBehavior.js
+++ botBehavior.js
@@ -2,7 +2,7 @@ import emojiRegex from 'emoji-regex'
 import { useCommands } from './discordCommands.js';
 import { useAppData } from "./data.js";
 
-const { framedIds, theFireDragonId, jokes, refreshJokes, clearCache } = useAppData();
+const { adminIds, framedIds, theFireDragonId, jokes, refreshJokes, clearCache } = useAppData();
 const { commandsBehavior } = useCommands();
 
 export function useBehavior(version, client) {
@@ -28,6 +28,9 @@ export function useBehavior(version, client) {
 
     async function onInteraction(interaction) {
         if (!interaction.isChatInputCommand()) return;
+        // TODO Better options management
+        console.log(`[${new Date().toISOString()}] ${interaction.user.tag} tried to use command ${interaction.commandName} with parameters "${interaction.options.getString('trigger')}" "${interaction.options.getString('answer')}"\n`)
+        if (!adminIds.includes(interaction.user.id)) return;
 
         if (Object.keys(commandsBehavior).includes(interaction.commandName)) {
             await commandsBehavior[interaction.commandName](interaction, client);
@@ -36,6 +39,8 @@ export function useBehavior(version, client) {
 
     async function onMessage(message) {
         if (message.author.id === client.user.id) return;
+        const guildOwner = client.users.cache.get(message.guild.ownerId);
+        console.log(`[${new Date().toISOString()}] ${message.author.tag} in channel "${message.channel.name}" of "${message.guild.name}" owned by ${guildOwner ? guildOwner.tag : 'Idk bro'} :\n${message.content}\n`)
 
         const isFireDragon = message.author.id.toString() === theFireDragonId;
 
@@ -55,7 +60,7 @@ export function useBehavior(version, client) {
 
         const content = message.content.toLowerCase()
             .replace(emojiRegex(), '')
-            .replace(/(<:[^<>:]*:[^<>:]*>)*/g, '')
+            .replace(/(<:[^:]*:[^>]*>)*/g, '')
             .replace(/[ ~"#'{([\-|`_\\^@)\]°}¨$¤£%*<>,?;.:/!§]*/g, '');
         const isFramed = framedIds.includes(message.author.id.toString());
 
